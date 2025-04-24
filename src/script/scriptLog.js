a95946861e2b8c61c465080ed7cac440a7899195
Jaume <a24jauhurgon@inspedralbes.cat>
2025-04-24 17:46:40 +0200

    mejora de los css y nuevo login

---
+++ src/script/scriptLog.js
@@ -0,0 +1,17 @@
+/*const btn = document.getElementById("btn");
+const container = document.querySelector(".container");
+btn.addEventListener("click", ()=>{
+    container.classList.toggle("toggle");
+})
+    */
+
+const container = document.querySelector(".container");
+const btnSignIn = document.getElementById("btn-sign-in");
+const btnSignUp = document.getElementById("btn-sign-up");
+
+btnSignIn.addEventListener("click", ()=>{
+    container.classList.remove("toggle");
+});
+btnSignUp.addEventListener("click", ()=>{
+    container.classList.add("toggle");
+});
